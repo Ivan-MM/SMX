3d13d0fb2e8e4ba277eb8e589383f8b6975c10b1
aperdiguerog <44448797+4lma@users.noreply.github.com>
2018-10-29 17:54:56 +0100

    Update ejercicio-edad-3.py

--- M_03/UF1/PYTHON/ejercicio-edad-3.py
+++ M_03/UF1/PYTHON/ejercicio-edad-3.py
@@ -1,2 +1,3 @@
-edad=int(input("que edad tiene tu teclado?"))
-if 
+edad = int(input("que edad tiene tu teclado?"))
+if (((edad >= 18)and(edad<=23))or(edad==17)):
+  print ("puedes entrar en la sesion de jovenes")
