5ad67dffbbba54302e7775700772d5302377ebc1
4lma <44448797+4lma@users.noreply.github.com>
2018-10-29 12:08:55 +0100

    Update python.py

--- M_03/UF1/python.py
+++ M_03/UF1/python.py
@@ -22,29 +22,13 @@ else:
     print ("tienes que pagar 2.5€")
     
 
-edad = 0;
-sexe = 1;          #0 és dona, 1 és home
-colorCabell = 0;
-precioFinal = 0;
-
-edad = int(input("Introduce tu edad: "))
-sexe = input("Introduce tu genero (M si eres mujer, H si eres hombre): ")
-colorCabell = int(input("Cual es el color de tu pelo? (1 Rubio/a, 2 Peliroja, 3 Morena, 0 para otro): "))
-jubilat = input("Estas jubilado? (si, no): ")
-
-if sexe == "H":
-    if jubilat == "no":
-        precioFinal == 1;
-
-if sexe == "M":
-    if jubilat == "no" and colorCabell == 1: 
-        precioFinal = 0;
-
-if sexe == "H, M":
-    if jubilat == "si" and edad >=65:
-        precioFinal = 0;
-
-if sexe == "M":
-    if jubilat == "no" and colorCabell == 0:
-        precioFinal = 0.5;
-
+    edad = int(input("que edad tienes?"))
+sexe = input("eres hombre o mujer?")
+color = int(input("Cual es el color de tu pelo? (1 Rubio/a, 2 Peliroja, 3 Morena, 0 para otro)"))
+jubilado=input("estas jubilado/a?")
+if ((sexe=="hombre")and(jubilado=="no")):
+    print("pagas 1€")
+if (((sexe=="mujer")and(color== 1)and(jubilado=="no"))or((jubilado=="si")and(edad>=65))):
+   print("entras gratis")
+if((sexe=="mujer")and(jubilado=="no")and(color==0)):
+   print("pagas 50 centimos")
