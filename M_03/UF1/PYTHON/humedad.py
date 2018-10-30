7162b8d0894188493968ece8b12cc2164943f26b
aperdiguerog <44448797+4lma@users.noreply.github.com>
2018-10-30 12:21:37 +0100

    Create humedad.py

---
+++ M_03/UF1/PYTHON/humedad.py
@@ -0,0 +1,5 @@
+humedad = int(input("Humedad: "))
+if humedad >= 80 :
+    print ("Si no te quieres mojar un paraguas debes de usar")
+else:
+print ("No temas, no necesitas usar el paraguas si no es de sombrilla")
