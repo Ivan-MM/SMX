d07a08e350f1c64952981af63e10be793d30abaa
aperdiguerog <44448797+4lma@users.noreply.github.com>
2018-11-08 11:51:35 +0100

    Create comparacion.py

---
+++ M_03/UF1/PYTHON/comparacion.py
@@ -0,0 +1,9 @@
+n1=int(input("introduce un número cualquiera"))
+n2=int(input("introduce un número cualquiera"))
+if(n1>n2):
+    print(n2,n1)
+else:
+    if(n2>n1):
+        print(n1,n2)
+    else:
+        print("son el mismo numero")
