92a259a11cdab3fbe1d15e8d682e6cccacc2a420
aperdiguerog <44448797+4lma@users.noreply.github.com>
2018-11-08 10:15:16 +0100

    Update entero.py

--- M_03/UF1/PYTHON/entero.py
+++ M_03/UF1/PYTHON/entero.py
@@ -1,3 +1,9 @@
+JUEGO DE PRUEBAS    
+Entrada        Sortida
+3             "positivo"
+-2            "negativo"
+0             "positivo"
+
 numero=int(input("elige un numero cualquiera"))
 if(numero<0):
   print("es negativo")
