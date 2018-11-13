bbaee74f5b10c0c3fc287b8ca7f0279c9fb8f665
aperdiguerog <44448797+4lma@users.noreply.github.com>
2018-11-13 12:52:10 +0100

    Update areas.py

--- M_03/UF1/PYTHON/areas.py
+++ M_03/UF1/PYTHON/areas.py
@@ -2,4 +2,6 @@ area= input("¿Que area quiere calcular?(Escriba T si es un triangulo o C si es 
 if (area=="T"):
   Tb=int(input("¿Cual es la base del tiangulo?"))
   Th=int(input("¿Cual es la altura del triangulo?"))
-         
+      if (((Tb*Th)/2)=respuesta):
+        print:
+          ("El area del triangulo es",respuesta)
