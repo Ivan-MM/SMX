fb5064e8d943802548fb4dedcebc9993d081d403
aperdiguerog <44448797+4lma@users.noreply.github.com>
2018-11-13 13:22:09 +0100

    Update areas.py

--- M_03/UF1/PYTHON/areas.py
+++ M_03/UF1/PYTHON/areas.py
@@ -1,7 +1,11 @@
+from math import pi
 area= input("¿Que area quiere calcular?(Escriba T si es un triangulo o C si es un cirulo)")
 if (area=="T"):
-  Tb=int(input("¿Cual es la base del tiangulo?"))
-  Th=int(input("¿Cual es la altura del triangulo?"))
-      if (((Tb*Th)/2)=respuesta):
-        print:
-          ("El area del triangulo es",respuesta)
+    Tb=float(input("¿Cual es la base del tiangulo?"))
+    Th=float(input("¿Cual es la altura del triangulo?"))
+    respuesta=((Tb*Th)/2)
+    print("El area del triangulo es","{0:.2f}".format(respuesta))
+if (area=="C"):
+    Cr=float(input("¿Cual es el radio del circulo?"))
+    respuesta2=((Cr**2)*pi)  
+    print("El area del triangulo es","{0:.2f}".format(respuesta2))
