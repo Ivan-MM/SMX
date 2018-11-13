9dd615c834f34f14dbfe7d196092018178f41122
aperdiguerog <44448797+4lma@users.noreply.github.com>
2018-11-13 13:40:18 +0100

    Update and rename areas.py to calculo-areas.py

--- M_03/UF1/PYTHON/calculo-areas.py
+++ M_03/UF1/PYTHON/calculo-areas.py
@@ -4,14 +4,14 @@ if (area=="T"):
     Tb=float(input("¿Cual es la base del tiangulo?"))
     Th=float(input("¿Cual es la altura del triangulo?"))
     respuesta=((Tb*Th)/2)
-    if(respuesta>0)
+    if(respuesta>0):
         print("El area del triangulo es","{0:.2f}".format(respuesta))
     else:
         print("Esa area es imposible")
 if (area=="C"):
     Cr=float(input("¿Cual es el radio del circulo?"))
     respuesta2=((Cr**2)*pi)
-    if(respuesta2>0)
+    if(respuesta2>0):
         print("El area del triangulo es","{0:.2f}".format(respuesta2))
     else:
         print("Esa area es imposible")
